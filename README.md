8dbaa4a4d824ac35e43888625676fc74678da5a8
Jason <gt07891@greattree.com.tw>
2023-01-12 17:03:05 +0800

    update

--- README.md
+++ README.md
@@ -9,8 +9,16 @@ npm i gt-template -g
 
 ## 建立專案
 ```shell=
-npx gt-template@[版本號] [projectName]
+npx gt-template@[版本號] [projectName] [template target]
 
-// ex. npx gt-template@0.0.7-beta Hello
+// ex. npx gt-template@0.0.7-beta-2 Hello
 
 ```
+### template target
+目前只有一版，預測為/template內容
+
+## NPM
+[NPM倉庫](https://www.npmjs.com/package/gt-template)
+
+---
+更新日: 2023.01.12
--- index.js
+++ index.js
@@ -2,11 +2,17 @@
 
 // command sample : npx gt-template [projectName]
 
-const spawn = require('cross-spawn');
 const fs = require('fs');
 const path = require('path');
 
+const TemplateMap = {
+    default: 'template',
+};
+
 const projectName = process.argv[2];
+
+const targetTemplate =
+    process.argv[3] && Object.keys(TemplateMap).includes(process.argv[3]) ? process.argv[3] : 'default';
 
 if (!projectName) {
     console.error('projectName is empty');
@@ -16,12 +22,12 @@ if (!projectName) {
 // step1 新增專案夾
 const currentDir = process.cwd();
 const projectDir = path.resolve(currentDir, projectName);
-console.log('projectDir',projectDir)
+console.log('projectDir', projectDir);
 fs.mkdirSync(projectDir, { recursive: true });
 
 // step2 從模板夾複製
-const templateDir = path.resolve(__dirname, 'template');
-console.log('templateDir',templateDir)
+const templateDir = path.resolve(__dirname, TemplateMap[targetTemplate]);
+console.log('templateDir', templateDir);
 fs.cpSync(templateDir, projectDir, { recursive: true });
 
 // step3 處理專案文件
@@ -31,10 +37,3 @@ fs.renameSync(path.join(projectDir, 'gitignore'), path.join(projectDir, '.gitign
 const projectPackageJson = require(path.join(projectDir, 'package.json'));
 projectPackageJson.name = projectName;
 fs.writeFileSync(path.join(projectDir, 'package.json'), JSON.stringify(projectPackageJson, null, 2));
-
-// step4 npm啟動
-// spawn.sync(`cd ${projectName}`);
-// spawn.sync('npm', ['install'], { stdio: 'inherit' });
-
-// console.log('Success! Your new project is ready.');
-// console.log(`Created ${projectName} at ${projectDir}`);
